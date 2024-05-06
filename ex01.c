64852c9d8999259bc4c6ac7e1a7d712e1de55dcd
Sibimat <rasorisibilla@gmail.com>
2024-05-06 14:56:29 +0000

    sibi

---
+++ ex01.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main()
+{
+    //VARIABILI//
+    int a;
+    float b = 2.5;
+    double c = 2.6;
+    
+    a = 2;
+    printf("a: %d mattencio\nb: %f", a, b);
+
+ 
+
+}
---
+++ ex02.c
@@ -0,0 +1,24 @@
+int main()
+{
+    
+    //VARIABILI//
+    int a;
+    int b;
+    float c = 6,756385;
+    float d = 9,576283;
+    double e = 12,7523752875275;
+    double f = 43,439476735;
+
+    a=23
+    b=45
+    print("somma: %f\n", a+b);
+    printf("differenza: %f\n", a-b);
+    print("moltiplicazione: %f\n", a*b);
+    printf("divisione: %f\n", a/b);
+
+
+
+
+
+}
+
